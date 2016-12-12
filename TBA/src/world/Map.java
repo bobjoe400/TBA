6fa904009b2af1487d8c224ba11c8dae7ea62865
Cooper <Cooper@SaschaMobile>
2016-12-12 14:33:01 -0800

    Made he map a little bit more readable.

--- TBA/src/world/Map.java
+++ TBA/src/world/Map.java
@@ -6,31 +6,35 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
-import java.util.Random;
 
 import util.Generate;
 import util.Location;
+import util.typeCheck;
 
 public class Map {
 	private Room[][] map;
 
 	public Map() {
-		map = new Room[48][48];
+		map = new Room[48][96];
 		try {
 			File file = new File("Resources/Map.tbe");
 			PrintWriter p = new PrintWriter(file);
 			for (int i = 0; i < 48; i++) {
-				for (int j = 0; j < 48; j++) {
+				p.print("|");
+				for (int j = 0; j < 96; j++) {
 					if (i == 0 || i == 47 || j == 0 || j == 95) {
 						p.print("##");
 						map[i][j] = new Room("Wall", "WA", "It's a wall.");
-					} else if(i == 23  && j == 47){
-						map[i][j] = util.typeCheck.checkRoom("Prison Cell");
-					}else{
+					} else if (i == 23 && j == 47) {
+						Room temp = typeCheck.checkRoom("Prison Cell");
+						map[i][j] = temp;
+						p.print(temp.getAbbv());
+					} else {
 						Room temp = Generate.room();
 						map[i][j] = temp;
-						p.println(temp.getAbbv());
+						p.print(temp.getAbbv());
 					}
+					p.print("|");
 				}
 				p.println();
 			}
@@ -69,8 +73,8 @@ public class Map {
 	public Room getRoom(Location loc) {
 		return map[loc.getX()][loc.getY()];
 	}
-	
-	public void setRoom(Location loc, Room rm){
+
+	public void setRoom(Location loc, Room rm) {
 		map[loc.getX()][loc.getY()] = rm;
 	}
 
